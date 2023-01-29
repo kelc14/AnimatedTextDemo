77b134aea908962f836cd9bdb884a6592cfcf773
kelsey <kc.breen3@gmail.com>
2023-01-29 11:01:13 -0500

    restrict colors to purples

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector('h1');
 
 function getRandomRGB (){
     const R = Math.floor(Math.random() *256)
-    const G = Math.floor(Math.random() *256)
+    const G = 0
     const B = Math.floor(Math.random() *256)
 
     return `rgb(${R}, ${G}, ${B})`;
